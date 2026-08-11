bc7fc767950e8cbd17df5f1be80457c9ac99ede2
Lucky Odi <lucky.odi@wiocc.net>
2026-08-11 20:04:24 +0100

    Add vercel dependencies for deplolyment

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 
 
 @app.get("/")
