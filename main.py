f7a2ac08e842786d24b9f5c98baca2b41e58777e
Lucky Odi <lucky.odi@wiocc.net>
2026-08-23 21:28:36 +0100

    Modify main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 
-
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse(
