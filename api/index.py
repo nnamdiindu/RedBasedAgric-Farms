747eba5577f4c39103bbc766637e5882189faec4
Lucky Odi <lucky.odi@wiocc.net>
2026-08-23 21:32:40 +0100

    Add requirements.txt

---
+++ api/index.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+import os
+
+app = FastAPI()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
