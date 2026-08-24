db0e67e11599487d9444dc392a2e74ca1c67805e
Lucky Odi <lucky.odi@wiocc.net>
2026-08-24 07:27:15 +0100

    comment out main.py

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from starlette.staticfiles import StaticFiles
-import os
+# from fastapi import FastAPI, Request
+# from fastapi.templating import Jinja2Templates
+# from starlette.staticfiles import StaticFiles
+# import os
 
-app = FastAPI()
+# app = FastAPI()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
-app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+# app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 
-@app.get("/")
-async def index(request: Request):
-    return templates.TemplateResponse(
-        request=request,
-        name="index.html"
-    )
+# @app.get("/")
+# async def index(request: Request):
+#     return templates.TemplateResponse(
+#         request=request,
+#         name="index.html"
+#     )
 
-@app.get("/about_us")
-async def about_us(request: Request):
-    return templates.TemplateResponse(
-        request=request,
-        name="about.html"
-    )  
+ 
