3218e01fd95c1879fe0b9db97911c603b8e406f8
Lucky Odi <lucky.odi@wiocc.net>
2026-09-06 09:25:13 +0100

    add more routes

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ import os
 
 app = FastAPI()
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
 
@@ -22,3 +22,52 @@ async def about_us(request: Request):
         request=request,
         name="about.html"
     ) 
+
+@app.get("/shop")
+async def shop(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="shop.html"
+    )
+
+@app.get("/product")
+async def shop(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="product.html"
+    )
+
+@app.get("/cart")
+async def cart(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="cart.html"
+    )
+
+@app.get("/checkout")
+async def cart(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="checkout.html"
+    )
+
+@app.get("/delivery")
+async def cart(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="delivery.html"
+    )
+
+@app.get("/payment")
+async def cart(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="payment.html"
+    )
+
+@app.get("/order_confirmation")
+async def order_confirmation(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="order_confirmation.html"
+    )
